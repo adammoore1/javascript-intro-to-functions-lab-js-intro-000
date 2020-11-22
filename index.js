afb0902420a59158ed796512f7b848c0ccac7ce2
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-22 19:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   return console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if  (string.isLowerCase() === string){
+  if  (string.toLowerCase()){
   return ("I can't hear you!");
 }
 if (string.isUpperCase()){
